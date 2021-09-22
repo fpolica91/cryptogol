3a2a71434e9abf61ea5703253940c0111221111f
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-09-21 21:26:22 -0400

    apollo con la choteria de mongo db

---
+++ src/graphql/schemas/User.Schema.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  wallet: {
+    type: String,
+  },
+});
+
+export default mongoose.model("Videos", UserSchema);
--- src/utils/buildSchema.ts
+++
@@ -1,8 +0,0 @@
-import {resolve} from "path"
-import { GraphQLSchema } from "graphql"
-import { buildSchema } from "type-graphql"
-
-export const schema =():Promise<GraphQLSchema> => buildSchema({
-  resolvers: [resolve('src/resolvers/**/*.{mutations,queries}.{ts,js}')],
-  emitSchemaFile: resolve('schema.gql'),
-})
