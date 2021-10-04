7a27c838b132db57bce72b94e4a382db82744689
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-10-03 20:34:43 -0400

    setting heroku

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -3,12 +3,15 @@ import mongoose from "mongoose";
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  user: "root",
-  pass: "root",
+  user: "cryptogol",
+  pass: "Gepo1991??",
 } as any;
 
 try {
-  mongoose.connect(" mongodb://user:pwd@127.0.0.1:26016/", options);
+  // mongodb://user:pwd@127.0.0.1:26016/
+  const connectionString =
+    "mongodb+srv://cryptogol:pwd@cluster0.lqbzv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  mongoose.connect(connectionString, options);
 } catch (error) {
   console.log(JSON.stringify(error));
 }
